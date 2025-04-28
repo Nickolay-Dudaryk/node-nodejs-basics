1f88f1deb741851d728937f0f7174b4f81eec999
Nick_d <nick.dudaryk@gmail.com>
2025-04-28 23:00:14 +0300

    feat: zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import { createReadStream, createWriteStream } from "fs";
+import { unlink } from "fs/promises";
+import { resolve } from "path";
+import { pipeline } from "stream/promises";
+import { createGzip } from "zlib";
+
 const compress = async () => {
-    // Write your code here 
+  try {
+    const filePath = resolve("src/zip/files/fileToCompress.txt");
+    const gzipedFilePath = resolve("src/zip/files/archive.gz");
+    const readStream = createReadStream(filePath);
+    const writeStream = createWriteStream(gzipedFilePath);
+    const gzip = createGzip();
+
+    await pipeline(readStream, gzip, writeStream);
+    await unlink(filePath);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import { createReadStream, createWriteStream } from "fs";
+import { unlink } from "fs/promises";
+import { resolve } from "path";
+import { pipeline } from "stream/promises";
+import { createGunzip } from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+    const gzipedFilePath = resolve("src/zip/files/archive.gz");
+    const unzipedFilePath = resolve("src/zip/files/fileToCompress.txt");
+    const readStream = createReadStream(gzipedFilePath);
+    const writeStream = createWriteStream(unzipedFilePath);
+    const decompress = createGunzip();
+
+    await pipeline(readStream, decompress, writeStream);
+    await unlink(gzipedFilePath);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await decompress();

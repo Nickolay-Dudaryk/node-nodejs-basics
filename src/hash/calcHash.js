c64364811a2ee1c1e6a8bedd9fd2c0e8c3fb9586
Nick_d <nick.dudaryk@gmail.com>
2025-04-28 19:22:10 +0300

    feat: hash tasks

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createReadStream } from "fs";
+import { resolve } from "path";
+import { createHash } from "crypto";
+import { pipeline } from "stream/promises";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = resolve("src/hash/files/fileToCalculateHashFor.txt");
+  const readStream = createReadStream(filePath);
+  const hash = createHash("sha256");
+
+  await pipeline(readStream, hash);
+
+  console.log(hash.digest("hex"));
 };
 
 await calculateHash();

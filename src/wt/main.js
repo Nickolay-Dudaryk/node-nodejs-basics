5b4c90337644a0c3916abc9df55b4a7b6985a9b1
Nick_d <nick.dudaryk@gmail.com>
2025-04-29 00:16:28 +0300

    feat: worker tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,41 @@
+import { Worker } from "worker_threads";
+import { resolve } from "path";
+import { cpus } from "os";
+
 const performCalculations = async () => {
-    // Write your code here
+  const workerFilePath = resolve("src/wt/worker.js");
+
+  const runWorker = (number) => {
+    return new Promise((resolveWorker) => {
+      const worker = new Worker(workerFilePath);
+
+      worker.postMessage(number);
+
+      worker.on("message", (message) => {
+        resolveWorker({ status: "resolved", data: message });
+      });
+
+      worker.on("error", () => {
+        resolveWorker({ status: "error", data: null });
+      });
+
+      worker.on("exit", (code) => {
+        if (code !== 0) {
+          resolveWorker({ status: "error", data: null });
+        }
+      });
+    });
+  };
+
+  const numberOfCores = cpus().length;
+  const promises = [];
+
+  for (let i = 0; i < numberOfCores; i++) {
+    promises.push(runWorker(10 + i));
+  }
+
+  const workerResults = await Promise.all(promises);
+  console.log(workerResults);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,17 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort } from "worker_threads";
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+// n should be received from main thread
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+// This function sends result of nthFibonacci computations to main thread
+const sendResult = (number) => {
+  try {
+    const result = nthFibonacci(number);
+    parentPort.postMessage(result);
+  } catch (err) {
+    parentPort.postMessage({ error: true, message: err.message });
+  }
 };
 
-sendResult();
+parentPort.on("message", (number) => sendResult(number));

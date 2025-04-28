ea46984e92a77bbddf3d76e1a73d9dac72395d34
Nick_d <nick.dudaryk@gmail.com>
2025-04-28 23:24:33 +0300

    feat: cp tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import { spawn } from "child_process";
+import { resolve } from "path";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const scriptPath = resolve("src/cp/files/script.js");
+
+  // FYI: stdio -> https://nodejs.org/api/child_process.html#optionsstdio
+  const childProcess = spawn("node", [scriptPath, ...args], {
+    stdio: ["pipe", "pipe", "inherit"],
+  });
+
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
+  childProcess.on("error", (err) => {
+    console.log(`Error: ${err}`);
+  });
+  childProcess.on("exit", (code) => {
+    console.log(`Process exited with code ${code}`);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument2"]);
